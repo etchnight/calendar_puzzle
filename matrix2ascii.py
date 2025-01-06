ae07b2e33f0c912849667faf4495fd75f5816b17
dualwind <dualwind@qq.com>
2025-01-06 12:44:23 +0800

    推送补丁测试

--- matrix2ascii.py
+++ matrix2ascii.py
@@ -71,7 +71,7 @@ def getNodes(matrix, i, j, asciiType=0):
 
 # asciiType
 # 0 适合在IDE中查看
-# 1 适合在md浏览器中查看
+# 1 适合在md浏览器中查看(gitee)
 # 2 显示数字，用于测试
 def matrix2ascii(matrix, asciiType=0):
     ascii = ""
@@ -84,7 +84,17 @@ def matrix2ascii(matrix, asciiType=0):
     return ascii
 
 
-if __name__ == "__main__":
+# 优化gitee显示，补丁方案
+def asciiNextLine(line):
+    result = ""
+    for char in line:
+        if char in ["╻", "┃", "┓", "┏", "┳", "┣", "┫", "╋"]:
+            result = result + "┃"
+        elif char in ["╹", "╸","╺", "┛", "┗", "┻", "■","━"]:
+            result = result + "■"
+    return result + "\n"
+
+def test(filePath):
     # 测试
     matrix = [
         [4, 3, 3, 9, 9, 0, 0],
@@ -94,7 +104,44 @@ if __name__ == "__main__":
         [1, 1, 1, 6, 7, 0, 5],
         [0, 0, 0, 0, 7, 5, 5],
     ]
-    ascii = matrix2ascii(matrix, 2)
+    ascii = matrix2ascii(matrix, 1)
     print(ascii)
-    with open("temp.md", "w+", encoding="utf-8") as f:
+    with open(filePath, "w+", encoding="utf-8") as f:
         f.write(ascii)
+
+
+def tempPatch(filePath):
+    with open(filePath, "r", encoding="utf-8") as f:
+        content = f.read().splitlines()
+    with open(filePath, "w+", encoding="utf-8") as f:
+        result=''
+        for line in content:
+            line = line + "\n"+asciiNextLine(line)
+            result = result + line
+        f.write(result)
+
+def tempPatchs():
+    import os
+    # 指定文件夹路径
+    folder_path = '已完成结果/2025'
+
+    # 获取文件夹下的所有文件和文件夹名称
+    entries = os.listdir(folder_path)
+
+    # 遍历并打印文件夹下的所有文件
+    for entry in entries:
+        # 获取完整的文件路径
+        full_path = os.path.join(folder_path, entry)
+        # 检查是否为文件
+        if os.path.isfile(full_path):
+            print(full_path)
+
+
+
+if __name__ == "__main__":
+    filePath = "temp.md"
+    #test("temp.md")
+    tempPatch("temp.md")
+
+
+
